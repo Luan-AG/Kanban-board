ff7bcbba5018314cd46015d16f67a7408c5a2cc2
Luan Goncalves <luanarcharde@gmail.com>
2021-03-29 14:12:02 +0200

    Remove unnecessary comments

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const addBtns = document.querySelectorAll('.add-btn:not(.solid)'); //Array with all the 'add item' buttons
-const saveItemBtns = document.querySelectorAll('.solid'); //Array with all the 'save item' buttons
-const addItemContainers = document.querySelectorAll('.add-container'); // Array with all the hidden 'add container'
-const addItems = document.querySelectorAll('.add-item'); // Array with all the <div> elements inside the 'add container'
-const closeBtns = document.querySelectorAll('.btn-close');// Array with all the <div .btn-close>
+const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
+const saveItemBtns = document.querySelectorAll('.solid'); 
+const addItemContainers = document.querySelectorAll('.add-container'); 
+const addItems = document.querySelectorAll('.add-item'); 
+const closeBtns = document.querySelectorAll('.btn-close');
 
-const listColumns = document.querySelectorAll('.drag-item-list'); //Array with all the <ul> below together.
-const backlogUL = document.getElementById('backlog-list');  //Backlog <ul> element
-const progressUL = document.getElementById('progress-list'); //Progress <ul> element
-const testingUL = document.getElementById('complete-list'); //Testing <ul> element
-const completeUL = document.getElementById('on-hold-list'); //Complete <ul> element
+const listColumns = document.querySelectorAll('.drag-item-list'); 
+const backlogUL = document.getElementById('backlog-list'); 
+const progressUL = document.getElementById('progress-list'); 
+const testingUL = document.getElementById('complete-list'); 
+const completeUL = document.getElementById('on-hold-list'); 
 
 
 // Items
-let updatedOnLoad = false; //False means that when we load the page the content from LS was not yet downloaded, when it turns true it means it was loaded and we don't run it again.
+let updatedOnLoad = false; 
 
 // Initialize Arrays for each column 
 let backlogListArray = [];
@@ -28,12 +28,12 @@ let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
-  if (localStorage.getItem('backlogItems')) { //if the localstorage already exists, get the values and parse into the empty arrays
+  if (localStorage.getItem('backlogItems')) { 
     backlogListArray = JSON.parse(localStorage.backlogItems);
     progressListArray = JSON.parse(localStorage.progressItems);
     testingListArray = JSON.parse(localStorage.completeItems);
     completeListArray = JSON.parse(localStorage.onHoldItems);
-  } else { // else use this template arrays
+  } else { 
     backlogListArray = ['Task One', 'Task Three'];
     progressListArray = ['Task Two'];
     testingListArray = ['Task Six', 'Task Seven'];
@@ -43,10 +43,10 @@ function getSavedColumns() {
 
 // Create/Update the localStorage Arrays
 function updateSavedColumns() {
-  listArrays = [backlogListArray, progressListArray, testingListArray, completeListArray]; //Defining the listArrays to all the columns arrays
-  const arrayNames = ['backlog', 'progress', 'complete', 'onHold']; // Define an array with names for each LS
+  listArrays = [backlogListArray, progressListArray, testingListArray, completeListArray]; 
+  const arrayNames = ['backlog', 'progress', 'complete', 'onHold']; 
   arrayNames.forEach((arrayName, index) => {
-    localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index])); // Set the LS with the name of each column and the respective items. 
+    localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index])); 
   });
 }
 
@@ -58,57 +58,52 @@ function filterArray(array) {
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
-  // List Item
+ 
   const binIcon = document.createElement('img');
   binIcon.setAttribute('src', 'images/bin.svg');
   binIcon.className = 'bin-icon';
   
-
   const listEl = document.createElement('li');
   listEl.textContent = item;
-  listEl.id = index;//get the position of the item in the array
+  listEl.id = index;
   listEl.classList.add('drag-item');
-  listEl.draggable = true; //means I can get the item
-  /* listEl.setAttribute('onfocusout', `updateItem(${index}, ${column})`); *///event listener to delete if empty or updated if changed
+  listEl.draggable = true; 
   listEl.setAttribute('ondragstart', 'drag(event)');
-  /* listEl.contentEditable = true; */
-  // Append
   columnEl.appendChild(listEl);
   listEl.appendChild(binIcon);
 }
 
 // Get items from LS and Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
 function updateDOM() {
-  // Check localStorage once
-  if (!updatedOnLoad) { //if the value of the variable 'updatedOnLoad' is false, then get the data from local storage, else skip it.
+  if (!updatedOnLoad) { 
     getSavedColumns();
   }
   // Populate the Backlog Column with the LS items
-  backlogUL.textContent = ''; //reset the ul just in case
-  backlogListArray.forEach((backlogItem, index) => { //Get all the items and index positions from the LS array
-    createItemEl(backlogUL, 0, backlogItem, index);//Populate the UI column for each item of the array from LS
+  backlogUL.textContent = ''; 
+  backlogListArray.forEach((backlogItem, index) => {
+    createItemEl(backlogUL, 0, backlogItem, index);
   });
 
   backlogListArray = filterArray(backlogListArray);
-  // Progress Column
+ 
   progressUL.textContent = '';
   progressListArray.forEach((progressItem, index) => {
     createItemEl(progressUL, 1, progressItem, index);
   });
   progressListArray = filterArray(progressListArray);
-  // Testing Column
+ 
   testingUL.textContent = '';
   testingListArray.forEach((completeItem, index) => {
     createItemEl(testingUL, 2, completeItem, index);
   });
   testingListArray = filterArray(testingListArray);
-  // Complete Column
+ 
   completeUL.textContent = '';
   completeListArray.forEach((onHoldItem, index) => {
     createItemEl(completeUL, 3, onHoldItem, index);
   });
   completeListArray = filterArray(completeListArray);
-  // Don't run more than once, Update Local Storage
+
   updatedOnLoad = true;
   updateSavedColumns();
 }
@@ -151,7 +146,7 @@ deleteEvents();
 
 
 
-// Update Item - Delete if necessary, or update Array value
+// Update Item 
 function updateItem(id, column) {
   const selectedArray = listArrays[column];
   const selectedColumn = listColumns[column].children;
@@ -257,7 +252,7 @@ function dragEnter(column) {
 
 // When Item Starts Dragging
 function drag(e) {
-  draggedItem = e.target;// Get the target element and store into the variable 'draggedItem'
+  draggedItem = e.target;
   dragging = true;
 }
 
@@ -270,13 +265,10 @@ function allowDrop(e) {
 function drop(e) {
   e.preventDefault();
   const parent = listColumns[currentColumn];
-  // Remove Background Color/Padding
   listColumns.forEach((column) => {
     column.classList.remove('over');
   });
-  // Add item to Column
   parent.appendChild(draggedItem);
-  // Dragging complete
   dragging = false;
   rebuildArrays();
 }
